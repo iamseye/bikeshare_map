aa00412c25e610ba2fcb6f0367c6fe535382379c
Jess <iamseye@gmail.com>
2019-09-10 11:16:30 -0400

    ADD: init files

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/header.scss';
+
+
+const Header = () => (
+  <header className="header">
+    <h3>
+      BIKE SHARE MAP
+    </h3>
+  </header>
+);
+
+export default Header;
---
+++ src/components/map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/map.scss';
+
+
+const Map = () => (
+  <div>
+    Map goes here
+  </div>
+);
+
+export default Map;
