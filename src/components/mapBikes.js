8a7bbdb6c8060aab9a4030997e06774e4a581c10
Jess <iamseye@gmail.com>
2019-09-11 11:38:49 -0400

    ADD: click marker action

--- src/components/mapBikes.js
+++ src/components/mapBikes.js
@@ -1,40 +1,64 @@
 import React, { Component } from 'react'
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react'
 import '../styles/mapBikes.scss'
-
 
 class MapBikes extends Component {
   constructor(props) {
     super(props)
     this.state = {
       montrealCenter: { lat: 45.516136, lng: -73.656830},
-      stations: []
-
+      stations: [],
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+      selectedPlaceName: ''
     }
   }
 
   componentDidMount () {
-    console.log(process.env.REACT_APP_GET_STATIONS_API)
-
     fetch(process.env.REACT_APP_GET_STATIONS_API)
       .then(res => res.json())
       .then(
-        (result) => {
+        result => {
           this.setState({
             stations: result.data.stations
           });
         },
-        (error) => {
+        error => {
           console.log(error)
         }
       )
   }
 
+  onMarkerClick = (props, marker, e) => {
+    this.getStationStatus(props.id)
+      .then(res => {
+        this.setState({
+          selectedPlace: res,
+          activeMarker: marker,
+          showingInfoWindow: true,
+          selectedPlaceName: props.name
+        })
+      })
+  }
+
+  async getStationStatus(id) {
+     const response =  await fetch(process.env.REACT_APP_GET_STATION_STATUS, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ id: parseInt(id) })
+        })
+      return await response.json()
+    }
+
   render() {
     return (
       <Map
         google={this.props.google}
-        zoom={11}
+        zoom={14}
         initialCenter={this.state.montrealCenter}
       >
         { this.state.stations.map((item, i) => (
@@ -42,9 +66,20 @@ class MapBikes extends Component {
              lat: item.lat,
              lng: item.lon
            }}
-           onClick={() => console.log("You clicked me!")} />
+           onClick={this.onMarkerClick} name={item.name} />
          ))
-       }
+        }
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h2>{this.state.selectedPlaceName}</h2>
+            <ul>
+              <li>Available Bikes: <p>{this.state.selectedPlace.num_bikes_available}</p></li>
+              <li>Free docks: <p>{this.state.selectedPlace.num_docks_available}</p></li>
+            </ul>
+          </div>
+        </InfoWindow>
       </Map>
     )
   }
